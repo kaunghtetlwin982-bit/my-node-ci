26a5df259fe0952dae971fe1d7af1cd273b013c7
kaunghtetlwin982-bit <kaunghtetlwin@gmail.com>
2026-04-01 16:53:02 +0630

    fix: restrict jest to src and enable experimental ESM

---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  // This tells Jest to ONLY look in the src folder
+  roots: ['<rootDir>/src/'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
